be95df60cefe412ab59086e0ab157c7eb6f4c645
Joel Hutton <joel.hutton@arm.com>
2016-08-10 14:47:32 +0100

    "removed programs I no longer use"

--- setup.sh
+++ setup.sh
@@ -37,24 +37,16 @@ prompt_install ubuntu-restricted-extras
 prompt_install chromium-browser
 prompt_install git 
 prompt_exec  'go to chrome web page' "sudo -u joel chromium-browser https://www.google.com/chrome/browser/desktop/"
-# 1. Add the Spotify repository signing key to be able to verify downloaded packages
-prompt_exec  'add spotiy repository key'  " apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59 D2C19886"
-# 2. Add the Spotify repository
-prompt_exec  'add spotify repository to sources' "echo deb http://repository.spotify.com stable non-free |  tee /etc/apt/sources.list.d/spotify.list"
-prompt_exec  'install spotify' " apt update&& apt-get install spotify-client"
-prompt_install git
-promp_exec 'configure git email and name?' 'git config --global user.email "joel-hutton@hotmail.com" && git config --global user.name "Joel Hutton"'
+prompt_exec 'configure git email and name?' 'git config --global user.email "joel-hutton@hotmail.com" && git config --global user.name "Joel Hutton"'
 prompt_install xclip 
 prompt_install xsel 
-prompt_install thunderbird
-prompt_install thunderbird-gnome-support
 prompt_exec 'get joels rc files from git?' 'cd /home/joel && git clone https://github.com/JoelHutton/vimrc.git && cd vimrc && ./setup.sh && cd .. && git clone https://github.com/JoelHutton/bashrc.git && cd bashrc && ./setup'
 prompt_exec "force all chromium icons to rescale to 1.67 times (to act like a 1080p screen)" "find / -regex '.*\\.desktop' | sudo xargs sed -i 's:Exec=.*chromium-browser:\\0\\ --force-device-scale-factor=1.67:g'"
 
 prompt_exec 'change default editor' 'sudo update-alternatives --config editor'
 for command in "${commands[@]}"
 do
-	echo $command
+	exec $command
 done
 
 echo 'remember to:
