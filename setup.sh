5a4f36fd44e625437f25ca208e9d0e213f4f8a61
Joel Hutton <joel.hutton@arm.com>
2018-01-03 16:06:23 +0000

    Added explode windows mouse button mapping

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -x 
+
 apt-get update
 apt-get upgrade
-#proprietary drivers
+#xps 13 specific
 #dpkg -i dkms_2.2.0.3-2ubuntu3_all.deb
+#cp BCM20702A1-0a5c-216f.hcd /lib/firmware/brcm/
 
 #install some useful programs
 apt-get install -y vim-gnome
@@ -21,13 +24,26 @@ apt-get install -y arduino
 apt-get install -y tlp
 apt-get install -y zsh
 chsh -s /usr/bin/zsh
-#get rc files from git?
-mkdir -p /home/joel/git && cd /home/joel/git && git clone https://github.com/JoelHutton/vimrc.git && cd vimrc && ./setup.sh && cd .. && git clone https://github.com/JoelHutton/bashrc.git && cd bashrc && ./setup && cd .. && git clone https://github.com/JoelHutton/zshrc.git && cd zshrc && ./setup.sh
+# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# 3. Update list of available packages
+sudo apt-get update
+# 4. Install Spotify
+sudo apt-get install spotify-client
+#get rc files from git
+git clone https://github.com/JoelHutton/vimrc.git
+git clone https://github.com/JoelHutton/zshrc.git
+#xbindkeys, you will need to change explode shortcut to ctrl \
+sudo apt install xbindkeys
+sudo apt install xautomation
+cp xbindkeysrc $HOME/.xbindkeysrc
+#make chrome always start in incognito
 sed -i -e 's/google-chrome-stable/google-chrome-stable\ --incognito/g' /usr/share/applications/google-chrome.desktop
 #change default editor
 sudo update-alternatives --config editor
 google-chrome-stable  https://www.privateinternetaccess.com/installer/download_installer_linux
-cp BCM20702A1-0a5c-216f.hcd /lib/firmware/brcm/
 echo '[Re-enable hibernate by default for login1]
   Identity=unix-user:*
   Action=org.freedesktop.login1.hibernate
@@ -37,4 +53,7 @@ echo '[Re-enable hibernate by default for login1]
   Identity=unix-user:*
   Action=org.freedesktop.login1.hibernate-multiple-sessions
   ResultActive=yes' > /etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla
-killall unity-panel-service
+#so caps and escape can be swapped
+sudo apt-get install gnome-tweak-tool -y && gnome-tweak-tool
+#Remember to enable workspaces, autohide taskbar, enable natural scrolling, 
+#tap to click
